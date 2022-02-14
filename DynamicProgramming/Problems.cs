627e9da05b282c01ec75d2a6298bdf3b968198c7
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2022-02-14 05:51:20 +0200

    Adding Fib and Grid Traveler

---
+++ DynamicProgramming/Problems.cs
@@ -0,0 +1,66 @@
+﻿namespace DynamicProgramming;
+public static class Problems
+{
+    #region Fib
+
+    #region classic
+
+    public static int FibSumRecursiveClassic(int n, int prev = 0, int secondPrev = 1, int count = 1)
+    {
+        if (count == n)
+            return prev + secondPrev;
+        var res = prev + secondPrev;
+        return res + FibSumRecursiveClassic(n, res, prev, count + 1);
+    }
+
+    public static long FibRecursiveClassic(long n, long prev = 0, long secondPrev = 1, long count = 1)
+    {
+        if (count == n)
+            return prev + secondPrev;
+        var res = prev + secondPrev;
+        return FibRecursiveClassic(n, res, prev, count + 1);
+    }
+
+    #endregion
+
+    #region DP
+
+    public static long FibRecursive(long n, Dictionary<long, long> memo = null)
+    {
+        memo ??= new();
+
+        if (memo.ContainsKey(n))
+            return memo[n];
+
+        if (n <= 2)
+            return 1;
+
+        memo[n] = FibRecursive(n - 1, memo) + FibRecursive(n - 2, memo);
+        return memo[n];
+    }
+
+    #endregion
+
+    #endregion
+
+    #region GridTraveler
+
+    public static long GetAllPathsToTravelGrid(int rows, int columns, Dictionary<(int r, int c), long> memo = null)
+    {
+        memo ??= new();
+
+        if (memo.ContainsKey((rows, columns)))
+            return memo[(rows, columns)];
+
+        if (rows == 0 || columns == 0)
+            return 0;
+        if (rows == 1 && columns == 1)
+            return 1;
+
+        memo[(rows, columns)] = GetAllPathsToTravelGrid(rows - 1, columns, memo) + GetAllPathsToTravelGrid(rows, columns - 1, memo);
+        return memo[(rows, columns)];
+    }
+
+    #endregion
+}
+
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DynamicProgramming;
+
+#region Testing 
+
+#region Fib
+
+Console.WriteLine(Problems.FibSumRecursiveClassic(8));
+Console.WriteLine(Problems.FibRecursiveClassic(8));
+Console.WriteLine(Problems.FibRecursiveClassic(50));
+Console.WriteLine(Problems.FibRecursive(50));
+
+#endregion
+
+#region Grid Traveler
+
+Console.WriteLine(Problems.GetAllPathsToTravelGrid(40,40));
+
+#endregion
+
+#endregion
