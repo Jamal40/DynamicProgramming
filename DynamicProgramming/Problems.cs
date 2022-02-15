a166f0ab5ed8b9d84db5ff38d433ccbc11f6d114
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2022-02-15 21:20:06 +0200

    Adding CanSum and HowSum problems

--- DynamicProgramming/Problems.cs
+++ DynamicProgramming/Problems.cs
@@ -51,6 +51,8 @@ public static class Problems
 
         if (memo.ContainsKey((rows, columns)))
             return memo[(rows, columns)];
+        if (memo.ContainsKey((columns, rows)))
+            return memo[(columns, rows)];
 
         if (rows == 0 || columns == 0)
             return 0;
@@ -59,6 +61,91 @@ public static class Problems
 
         memo[(rows, columns)] = GetAllPathsToTravelGrid(rows - 1, columns, memo) + GetAllPathsToTravelGrid(rows, columns - 1, memo);
         return memo[(rows, columns)];
+    }
+
+    #endregion
+
+    #region CanSum
+
+    public static bool CanSum(int sum, int[] numbers, Dictionary<int, bool> memo = null)
+    {
+        memo ??= new();
+
+        if (memo.ContainsKey(sum))
+            return memo[sum];
+        if (sum == 0)
+            return true;
+        if (sum < 0)
+            return false;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var newSum = sum - numbers[i];
+            memo[newSum] = CanSum(newSum, numbers, memo);
+            if (memo[newSum])
+                return true;
+        }
+        return false;
+    }
+
+    #endregion
+
+    #region HowSum
+
+    public static List<int> HowSum(int sum, int[] numbers)
+    {
+        var result = new List<int>();
+        HowSumHelper(sum, numbers, result);
+        return result;
+    }
+
+    private static bool HowSumHelper(int sum, int[] numbers, List<int> result, Dictionary<int, bool> memo = null)
+    {
+        memo ??= new();
+
+        if (memo.ContainsKey(sum))
+            return memo[sum];
+        if (sum == 0)
+            return true;
+        if (sum < 0)
+            return false;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int newSum = sum - numbers[i];
+            memo[newSum] = HowSumHelper(newSum, numbers, result, memo);
+            if (memo[newSum])
+            {
+                result.Add(numbers[i]);
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static List<int> HowSum2(int sum, int[] numbers, Dictionary<int, List<int>> memo = null)
+    {
+        memo ??= new();
+
+        if (memo.ContainsKey(sum))
+            return memo[sum];
+        if (sum == 0)
+            return new List<int> ();
+        if (sum < 0)
+            return null;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int newSum = sum - numbers[i];
+            memo[newSum] = HowSum2(newSum, numbers, memo);
+            if (memo[newSum] is not null)
+            {
+                memo[newSum].Add(numbers[i]);
+                return memo[newSum];
+            }
+        }
+
+        return null;
     }
 
     #endregion
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -4,6 +4,7 @@
 
 #region Fib
 
+Console.WriteLine("----------Fib-----------");
 Console.WriteLine(Problems.FibSumRecursiveClassic(8));
 Console.WriteLine(Problems.FibRecursiveClassic(8));
 Console.WriteLine(Problems.FibRecursiveClassic(50));
@@ -13,8 +14,49 @@ Console.WriteLine(Problems.FibRecursive(50));
 
 #region Grid Traveler
 
-Console.WriteLine(Problems.GetAllPathsToTravelGrid(40,40));
+Console.WriteLine("---------Grid Traveler---------");
+Console.WriteLine(Problems.GetAllPathsToTravelGrid(40, 40));
+Console.WriteLine(Problems.GetAllPathsToTravelGrid(3, 3));
+Console.WriteLine(Problems.GetAllPathsToTravelGrid(18, 18));
+Console.WriteLine(Problems.GetAllPathsToTravelGrid(2, 1));
+Console.WriteLine(Problems.GetAllPathsToTravelGrid(1, 2));
 
 #endregion
+
+#region CanSum
+
+Console.WriteLine(Problems.CanSum(7, new int[] { 4, 2 })); // false
+Console.WriteLine(Problems.CanSum(7, new int[] { 3, 2 })); // true
+Console.WriteLine(Problems.CanSum(7, new int[] { 5, 4, 3, 2 })); // true
+Console.WriteLine(Problems.CanSum(8, new int[] { 2, 5, 3 })); // true
+Console.WriteLine(Problems.CanSum(5000, new int[] { 7, 14 })); // false
+
+#endregion
+
+#region HowSum
+
+PrintCollection(Problems.HowSum2(7, new int[] { 4, 2 }));
+PrintCollection(Problems.HowSum2(7, new int[] { 3, 2 }));
+PrintCollection(Problems.HowSum2(7, new int[] { 5, 4, 3, 2 }));
+PrintCollection(Problems.HowSum2(8, new int[] { 2, 5, 3 }));
+PrintCollection(Problems.HowSum2(300, new int[] { 7, 14 }));
+
+#endregion
+
+#endregion
+
+#region Helpers
+
+void PrintCollection<T>(ICollection<T> collection, string text = "")
+{
+    if (collection is null || !collection.Any())
+        return;
+    Console.WriteLine($"------------Start_{text}-----------");
+    foreach (var item in collection)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine($"------------End_{text}-----------");
+}
 
 #endregion
